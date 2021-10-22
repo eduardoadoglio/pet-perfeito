b5d60261a38e7cc3620d680b5a5893f890f0f7ba
Inutilismo <gabriel.affonso@usp.br>
2021-10-21 22:39:54 -0300

    Primeiro commit
    Estrutura do projeto (Spring)

---
+++ backend/src/main/java/com/esi/petperfeito/PetPerfeitoApplication.java
@@ -0,0 +1,13 @@
+package com.esi.petperfeito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetPerfeitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetPerfeitoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/esi/petperfeito/PetPerfeitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esi.petperfeito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetPerfeitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
