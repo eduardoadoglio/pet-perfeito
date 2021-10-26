5d19c5ecc6db5759da03019646c8644944075fb1
Juan Carlos Elias Obando Valdivia <juan.valdivia@usp.br>
2021-10-26 18:03:05 -0300

    Ong.java modificado

--- backend/src/main/java/com/esi/petperfeito/model/Ong.java
+++ backend/src/main/java/com/esi/petperfeito/model/Ong.java
@@ -1,112 +1,96 @@
-package com.esi.petperfeito.controller;
+package com.esi.petperfeito.model;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import javax.persistence.*;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+@Entity
+@Table(name = "ongs")
+public class Ong {
 
-import com.esi.petperfeito.model.Ong;
-import com.esi.petperfeito.repository.OngRepository;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
-@CrossOrigin(origins = "http://localhost:8081")
-@RestController
-@RequestMapping("/api")
-public class OngController {
+    @Column(name = "nome")
+    private String nome;
 
-    @Autowired
-    OngRepository ongRepository;
+    @Column(name = "cpf")
+    private String cpf;
 
-    @GetMapping("/ongs")
-    public ResponseEntity<List<Ong>> getAllOngs(@RequestParam(required = false) String nome) {
-        try {
-            List<Ong> ongs = new ArrayList<Ong>();
+    @Column(name = "telefone")
+    private String telefone;
 
-            if (nome == null)
-                ongRepository.findAll().forEach(ongs::add);
-            else
-                ongRepository.findByNome(nome).forEach(ongs::add);
+    @Column(name = "cep")
+    private String cep;
 
-            if (ongs.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
+    @Column(name = "data_nascimento")
+    private String dataNascimento;
 
-            return new ResponseEntity<>(ongs, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @GetMapping("/ongs/{id}")
-    public ResponseEntity<Ong> getOngById(@PathVariable("id") long id) {
-        Optional<Ong> ongData = ongRepository.findById(id);
-
-        if (ongData.isPresent()) {
-            return new ResponseEntity<>(ongData.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @PostMapping("/ongs")
-    public ResponseEntity<Ong> createOng(@RequestBody Ong ong) {
-        try {
-            Ong _ong = ongRepository
-                    .save(new Ong(ong.getNome(), ong.getCpf(), ong.getTelefone(), ong.getCep(), ong.getDataNascimento()));
-            return new ResponseEntity<>(_ong, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @PutMapping("/ongs/{id}")
-    public ResponseEntity<Ong> updateOng(@PathVariable("id") long id, @RequestBody Ong ong) {
-        Optional<Ong> ongData = ongRepository.findById(id);
-
-        if (ongData.isPresent()) {
-            Ong _ong = ongData.get();
-            _ong.setNome(ong.getNome());
-            _ong.setCpf(ong.getCpf());
-            _ong.setTelefone(ong.getTelefone());
-            _ong.setCep(ong.getCep());
-            _ong.setDataNascimento(ong.getDataNascimento());
-            return new ResponseEntity<>(ongRepository.save(_ong), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @DeleteMapping("/ongs/{id}")
-    public ResponseEntity<HttpStatus> deleteOng(@PathVariable("id") long id) {
-        try {
-            ongRepository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @DeleteMapping("/ongs")
-    public ResponseEntity<HttpStatus> deleteAllOngs() {
-        try {
-            ongRepository.deleteAll();
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public Ong() {
 
     }
 
+    public Ong(String nome, String cpf, String telefone, String cep, String dataNascimento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.cep = cep;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Ong{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", cep='" + cep + '\'' +
+                ", dataNascimento='" + dataNascimento + '\'' +
+                '}';
+    }
 }
